c122f7c0012a98c38c06ee7e6f412850187368b1
Mark Snyder <mark.clifford.snyder@gmail.com>
2023-12-17 23:32:07 -0500

    day 12 star 2 needed caching and input row reduction

--- Day12/hot_springs.py
+++ Day12/hot_springs.py
@@ -1,116 +1,42 @@
 from argparse import ArgumentParser
-import concurrent.futures
-import re
+from functools import cache
 
-def all_ints(s):
-    return tuple(int(i) for i in re.findall(r'\b\d+\b', s))
-
-def find_broken_sequences(row):
-    sequences = []
-    sequence = 0
-    for char in row:
-        if char == '?':
-            break
-        elif char == '#':
-            sequence += 1
-        else:
-            if sequence:
-                sequences.append(sequence)
-                sequence = 0
-    if sequence:
-        sequences.append(sequence)
-    return tuple(sequences)
-
-def find_solns(row, sequence_lengths, solns):
-    unsolved = ''.join(row).find('?')
-    if unsolved == -1:
-        if find_broken_sequences(row) == sequence_lengths:
-            solns.append(row)
-        return
-    for poss_seq, true_seq in zip(find_broken_sequences(row)[:-1], sequence_lengths):
-        if poss_seq != true_seq:
-            return
-    if row.count('?') + row.count('#') < sum(sequence_lengths):
-        return
-    if row.count('?') + row.count('.') < len(sequence_lengths) - 1:
-        return
-    fixed_path, broken_path = list(row), list(row)
-    fixed_path[unsolved] = '.'
-    broken_path[unsolved] = '#'
-    find_solns(fixed_path, sequence_lengths, solns)
-    find_solns(broken_path, sequence_lengths, solns)
-
-def count_solns(row, sequence_lengths):
-    unsolved = ''.join(row).find('?')
-    if unsolved == -1:
-        return 1 if find_broken_sequences(row) == sequence_lengths else 0
-    for poss_seq, true_seq in zip(find_broken_sequences(row)[:-1], sequence_lengths):
-        if poss_seq != true_seq:
-            return 0
-    if row.count('?') + row.count('#') < sum(sequence_lengths):
-        return 0
-    if row.count('?') + row.count('.') < len(sequence_lengths) - 1:
-        return 0
-    fixed_path, broken_path = list(row), list(row)
-    fixed_path[unsolved] = '.'
-    broken_path[unsolved] = '#'
-    return count_solns(fixed_path, sequence_lengths) + count_solns(broken_path, sequence_lengths)
-
-def solve_line(line):
-    spring_row = line.split()[0]
-    sequence_lengths = all_ints(line)
-    return count_solns(spring_row, sequence_lengths)
-
-def solve_extend_line(line):
-    spring_row = line.split()[0]
-    spring_row = '?'.join([spring_row]*5)
-    sequence_lengths = all_ints(line)*5
-    return count_solns(spring_row, sequence_lengths)
-
-def solve_extend_line_experiment(line):
-    spring_row = line.split()[0]
-    sequence_lengths = all_ints(line)
-    base_solns = []
-    find_solns(spring_row, sequence_lengths, base_solns)
-
-    if len(base_solns) == 1 and base_solns[0][0] == '#' and base_solns[0][-1] == '#':
+@cache
+def count_solns(row, sequence_lengths, curr_streak=0):
+    if not sequence_lengths and not row:
         return 1
-
-    prepend_spring_row = '?' + spring_row
-    append_spring_row = spring_row + '?'
-    additional_solns = max(count_solns(prepend_spring_row,sequence_lengths), count_solns(append_spring_row, sequence_lengths))
-    return len(base_solns) * additional_solns**4
+    if not sequence_lengths:
+        if row.count('#') == 0:
+            return 1
+        return 0
+    if not row:
+        if len(sequence_lengths) == 1 and curr_streak == sequence_lengths[0]:
+            return 1
+        return 0
+    if curr_streak > sequence_lengths[0]:
+        return 0
+    match row[0]:
+        case '.':
+            if curr_streak:
+                return 0 if curr_streak != sequence_lengths[0] else count_solns(row[1:], sequence_lengths[1:], 0)
+            else:
+                return count_solns(row[1:], sequence_lengths, 0)
+        case '#':
+            return count_solns(row[1:], sequence_lengths, curr_streak+1)
+        case '?':
+            return (count_solns('.' + row[1:], sequence_lengths, curr_streak) 
+                    + count_solns('#' + row[1:], sequence_lengths, curr_streak))
 
 def main(data):
-    # Star 1
     arrangements = 0
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        future_to_line = {executor.submit(solve_line, line): (idx,line) for (idx,line) in enumerate(data)}
-        for future in concurrent.futures.as_completed(future_to_line):
-            idx,line = future_to_line[future]
-            try:
-                solns = future.result()
-            except Exception as exc:
-                print('%s generated an exception: %s' % (line, exc))
-            else:
-                #print('%s (line %d) has %d possible solns' % (line, idx, solns))
-                arrangements += solns
-    print(f'{arrangements} possible solutions for star 1\n')
-
-    # Star 2
-    arrangements = 0
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        future_to_line = {executor.submit(solve_extend_line_experiment, line): (idx,line) for (idx,line) in enumerate(data)}
-        for future in concurrent.futures.as_completed(future_to_line):
-            idx,line = future_to_line[future]
-            try:
-                solns = future.result()
-            except Exception as exc:
-                print('%s generated an exception: %s' % (line, exc))
-            else:
-                print('%s (line %d) has %d possible solns' % (line, idx, solns))
-                arrangements += solns
-    print(f'{arrangements} possible solutions for star 2')
+    arrangements2 = 0
+    for line in data:
+        row, sequence_lengths = line.split()
+        sequence_lengths = tuple(int(length) for length in sequence_lengths.split(','))
+        arrangements += count_solns(row, sequence_lengths)
+        arrangements2 += count_solns('?'.join([row]*5), sequence_lengths*5)
+    print(arrangements)
+    print(arrangements2)
 
 def read_input(input_file):
     data = []
