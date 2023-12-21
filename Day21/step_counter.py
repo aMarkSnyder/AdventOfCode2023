0748249ce29f026567d448562b5869f76fb908aa
Mark Snyder <mark.clifford.snyder@gmail.com>
2023-12-21 03:06:36 -0500

    axis directions seem to be working

--- Day21/step_counter.py
+++ Day21/step_counter.py
@@ -46,6 +46,22 @@ def count_parity(distances):
     even, odd = np.sum((distances!=-1).astype(int) * (1-distances%2)), np.sum((distances!=-1).astype(int) * distances%2)
     return even, odd
 
+def count_axis(axis_grid, step_goal, parity_counts):
+    # Returns the number of valid tiles either n/e/s/w and number of additional grids required
+    size = axis_grid.shape[0]
+    parity = step_goal % 2
+
+    # We need to add enough grids to make it so the step goal is in the outside grid
+    added_grids = (step_goal - np.min(axis_grid[axis_grid>=0])) // size
+    offset = added_grids * size
+    axis_grid = axis_grid + offset*np.ones_like(axis_grid)*(axis_grid>=0).astype(int)
+    count = count_reachable(axis_grid, step_goal)
+    if added_grids % 2:
+        count += added_grids//2 * parity_counts[parity]
+        count += (added_grids//2 + 1) * parity_counts[1-parity]
+    else:
+        count += added_grids * (parity_counts[0] + parity_counts[1]) // 2
+    return count, added_grids
 
 def main(data):
     array = [[char for char in line] for line in data]
@@ -67,8 +83,10 @@ def main(data):
     print(exactly_64)
 
     # Star 2
+    step_goal = 50
+    size = maze.height
     buffered_data = np.tile(data, (3,3))
-    buffered_start = (start[0]+2*maze.height, start[1]+2*maze.width)
+    buffered_start = (start[0]+size, start[1]+size)
     buffered_distances = calculate_distance(Maze(buffered_data), buffered_start)
     # for coord in [(0,0),(11,0),(22,0),(0,11),(11,11),(22,11),(0,22),(11,22),(22,22)]:
     #     buffered_distances[coord] += 100
@@ -78,6 +96,24 @@ def main(data):
     # position in the next block in any direction within a quadrant. It only gets messy when you cross an axis going
     # through the starting position. This means that for each quadrant, the outermost blocks are identical.
     # If the grid size is odd (it is), the tiles that are reachable for filled grids will flip each grid.
+    dist_grids = {}
+    for row in range(3):
+        for col in range(3):
+            dist_grids[(row-1,col-1)] = grid_index(buffered_distances, (row,col), size)
+
+    start_even, start_odd = count_parity(dist_grids[(0,0)])
+    parity = step_goal % 2
+    parity_counts = {0: start_even, 1: start_odd}
+
+    total_valid = count_reachable(distances, step_goal)
+    dir_lens = {}
+    for offset in [(-1,0),(1,0),(0,1),(0,-1)]:
+        valid, added_grids = count_axis(dist_grids[offset], step_goal, parity_counts)
+        total_valid += valid
+        dir_lens[offset] = added_grids
+    print(total_valid)
+    print(dir_lens)
+
     
 
 
