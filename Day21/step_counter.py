99c8a625eef9d245cfa47db1a631c8e5ab3d8095
Mark Snyder <mark.clifford.snyder@gmail.com>
2023-12-21 02:14:47 -0500

    saving day 21 star 2 notes

--- Day21/step_counter.py
+++ Day21/step_counter.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import numpy as np
+np.set_printoptions(linewidth=300, threshold=10000)
 
 class Maze:
     def __init__(self, data) -> None:
@@ -18,7 +19,7 @@ class Maze:
         return neighbors
 
 def calculate_distance(grid: Maze, start, steps=64):
-    distances = np.inf * np.ones(grid.data.shape)
+    distances = -1 * np.ones(grid.data.shape, dtype=int)
     distances[start] = 0
 
     queue = [start]
@@ -26,20 +27,36 @@ def calculate_distance(grid: Maze, start, steps=64):
         curr = queue.pop(0)
         neighbors = grid.neighbors(curr)
         for neighbor in neighbors:
-            if distances[neighbor] == np.inf:
+            if distances[neighbor] == -1:
                 distances[neighbor] = distances[curr] + 1
                 queue.append(neighbor)
     
     return distances
+
+def grid_index(grid, loc, base_size):
+        return grid[base_size*loc[0]:base_size*(loc[0]+1), base_size*loc[1]:base_size*(loc[1]+1)]
+    
+def count_reachable(distances, step_goal):
+    if step_goal % 2:
+        return np.sum((distances!=-1).astype(int) * distances%2 * (distances<=step_goal).astype(int))
+    else:
+        return np.sum((distances!=-1).astype(int) * (1-distances%2) * (distances<=step_goal).astype(int))
+    
+def count_parity(distances):
+    even, odd = np.sum((distances!=-1).astype(int) * (1-distances%2)), np.sum((distances!=-1).astype(int) * distances%2)
+    return even, odd
 
 
 def main(data):
     array = [[char for char in line] for line in data]
     data = np.array(array)
 
-    maze = Maze(data)
-    start = np.where(maze.data == 'S')
+    start = np.where(data == 'S')
     start = (start[0][0], start[1][0])
+    data[start] = '.'
+
+    # Star 1
+    maze = Maze(data)
     distances = calculate_distance(maze, start)
 
     exactly_64 = 0
@@ -48,6 +65,21 @@ def main(data):
             if distances[row,col] < 65 and not distances[row,col] % 2:
                 exactly_64 += 1
     print(exactly_64)
+
+    # Star 2
+    buffered_data = np.tile(data, (3,3))
+    buffered_start = (start[0]+2*maze.height, start[1]+2*maze.width)
+    buffered_distances = calculate_distance(Maze(buffered_data), buffered_start)
+    # for coord in [(0,0),(11,0),(22,0),(0,11),(11,11),(22,11),(0,22),(11,22),(22,22)]:
+    #     buffered_distances[coord] += 100
+    # print(buffered_distances)
+
+    # In the infinite grid, it takes a constant number of steps (the grid size) to move from one block to the same
+    # position in the next block in any direction within a quadrant. It only gets messy when you cross an axis going
+    # through the starting position. This means that for each quadrant, the outermost blocks are identical.
+    # If the grid size is odd (it is), the tiles that are reachable for filled grids will flip each grid.
+    
+
 
 def read_input(input_file):
     data = []
